b4fca68d6f01c171b3f6de62a481376e2a334881
Evan <evan@lrdesign.com>
2010-09-09 19:53:53 -0700

    initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LrdCms::Application.config.secret_token = '3d213b3c02ec8fc324f01d92c5f7c21da3f7c16c5f6e5b4a67c9832d1a51b0444d954824b72b152118a37d44abfe2692a4dea454e2f69086a772a35d52c4cd61'
