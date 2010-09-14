7a1977efdf572b482d29980d7e6fb277f856dd35
Evan <evan@lrdesign.com>
2010-09-14 16:40:48 -0700

    added first version of view generators for haml

--- lib/generators/haml/scaffold/scaffold_generator.rb
+++ lib/generators/haml/scaffold/scaffold_generator.rb
@@ -1,4 +1,20 @@
 class MyHamlScaffoldGenerator < Rails::Generators::NamedBase 
   p "defining the haml scaffold generator"
-  
+
+  def create_root_folder
+    empty_directory File.join("app/views", controller_file_path)
+  end
+
+  def copy_view_files
+    available_views.each do |view|
+      filename = filename_with_extensions(view)
+      template filename, File.join("app/views", controller_file_path, filename)
+    end
+  end
+
+  protected
+
+    def available_views
+      %w(index edit show new _form)
+    end
 end
