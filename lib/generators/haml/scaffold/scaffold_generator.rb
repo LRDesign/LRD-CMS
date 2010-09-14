8c2827faeb52785bba10620d060cd74748ec7e3c
Evan <evan@lrdesign.com>
2010-09-14 16:48:45 -0700

    attempt to fix namespacing in haml template generator

--- lib/generators/haml/scaffold/scaffold_generator.rb
+++ lib/generators/haml/scaffold/scaffold_generator.rb
@@ -1,20 +1,28 @@
-class MyHamlScaffoldGenerator < Rails::Generators::NamedBase 
-  p "defining the haml scaffold generator"
+module LRD
+  module Generators
+    class Haml < Rails::Generators::Base 
+      p "defining the haml scaffold generator"
+      
+      include Rails::Generators::ResourceHelpers
 
-  def create_root_folder
-    empty_directory File.join("app/views", controller_file_path)
-  end
+      def create_root_folder
+        empty_directory File.join("app/views", controller_file_path)
+      end
 
-  def copy_view_files
-    available_views.each do |view|
-      filename = filename_with_extensions(view)
-      template filename, File.join("app/views", controller_file_path, filename)
+      def copy_view_files
+        available_views.each do |view|
+          filename = filename_with_extensions(view)
+          template filename, File.join("app/views", controller_file_path, filename)
+        end
+      end
+
+      protected
+
+        def available_views
+          %w(index edit show new _form)
+        end
     end
+  
+    p MyHamlScaffoldGenerator.namespace
   end
-
-  protected
-
-    def available_views
-      %w(index edit show new _form)
-    end
 end
