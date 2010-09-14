ea613d7fd0c8473ec4829305b8781770ae5e681d
Evan <evan@lrdesign.com>
2010-09-14 15:12:34 -0700

    edited controller scaffold to remove unnecessary XML crap

--- lib/templates/rails/scaffold_controller/controller.rb
+++ lib/templates/rails/scaffold_controller/controller.rb
@@ -12,22 +12,13 @@ class <%= controller_class_name %>Controller < ApplicationController
   # GET <%= route_url %>/1
   def show
     @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @<%= singular_table_name %> }
-    end
   end
 
   # GET <%= route_url %>/new
-  # GET <%= route_url %>/new.xml
   def new
     @<%= singular_table_name %> = <%= orm_class.build(class_name) %>
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @<%= singular_table_name %> }
-    end
+    format.html # new.html.erb
   end
 
   # GET <%= route_url %>/1/edit
@@ -36,46 +27,32 @@ class <%= controller_class_name %>Controller < ApplicationController
   end
 
   # POST <%= route_url %>
-  # POST <%= route_url %>.xml
   def create
     @<%= singular_table_name %> = <%= orm_class.build(class_name, "params[:#{singular_table_name}]") %>
 
-    respond_to do |format|
-      if @<%= orm_instance.save %>
-        format.html { redirect_to(@<%= singular_table_name %>, :notice => '<%= human_name %> was successfully created.') }
-        format.xml  { render :xml => @<%= singular_table_name %>, :status => :created, :location => @<%= singular_table_name %> }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @<%= orm_instance.errors %>, :status => :unprocessable_entity }
-      end
+    if @<%= orm_instance.save %>
+      redirect_to(@<%= singular_table_name %>, :notice => '<%= human_name %> was successfully created.') 
+    else
+      render :action => "new" 
     end
   end
 
   # PUT <%= route_url %>/1
-  # PUT <%= route_url %>/1.xml
   def update
     @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
 
-    respond_to do |format|
       if @<%= orm_instance.update_attributes("params[:#{singular_table_name}]") %>
-        format.html { redirect_to(@<%= singular_table_name %>, :notice => '<%= human_name %> was successfully updated.') }
-        format.xml  { head :ok }
+        redirect_to(@<%= singular_table_name %>, :notice => '<%= human_name %> was successfully updated.')
       else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @<%= orm_instance.errors %>, :status => :unprocessable_entity }
+        render :action => "edit" 
       end
-    end
   end
 
   # DELETE <%= route_url %>/1
-  # DELETE <%= route_url %>/1.xml
   def destroy
     @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
     @<%= orm_instance.destroy %>
 
-    respond_to do |format|
-      format.html { redirect_to(<%= index_helper %>_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(<%= index_helper %>_url)
   end
 end
