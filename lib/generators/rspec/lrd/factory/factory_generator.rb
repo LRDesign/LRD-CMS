5b6b752442570af932a5d07211bb9ebfd8afe137
Evan Dorn <evan@lrdesign.com>
2010-09-15 09:34:04 -0700

    added default value modulegs

--- lib/generators/rspec/lrd/factory/factory_generator.rb
+++ lib/generators/rspec/lrd/factory/factory_generator.rb
@@ -1,38 +1,39 @@
+require File.dirname(__FILE__)+'/rspec_default_values'
 module Rspec
-module Lrd
-  module Generators
-    class Factory < Rails::Generators::NamedBase 
-      p "defining the factory scaffold generator"
+  module Lrd
+    module Generators
+      class Factory < Rails::Generators::NamedBase 
+        p "defining the factory scaffold generator"
     
-      include Rails::Generators::ResourceHelpers
-      source_root File.dirname(__FILE__) + '/templates'
+        include Rails::Generators::ResourceHelpers
+        source_root File.dirname(__FILE__) + '/templates'
       
-      argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"        
+        argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"        
 
-      def create_root_folder
-        empty_directory File.join("spec/factories")
-      end
+        def create_root_folder
+          empty_directory File.join("spec/factories")
+        end
 
-      def copy_factory_file
-        p "creating factory file with #{controller_file_name}"
-        filename = "#{controller_file_name}_factory.rb"        
-        template 'factory.rb', File.join("spec/factories", controller_file_path, filename)
-        say "templating filename #{filename}"          
-      end
+        def copy_factory_file
+          p "creating factory file with #{controller_file_name}"
+          filename = "#{controller_file_name}_factory.rb"        
+          template 'factory.rb', File.join("spec/factories", controller_file_path, filename)
+          say "templating filename #{filename}"          
+        end
 
-      protected
+        protected
 
-      def available_views
-        %w(index edit show new _form)
-      end
+        def available_views
+          %w(index edit show new _form)
+        end
       
-      def filename_with_extensions(name)
-        "#{name}.html.haml"
-      end
+        def filename_with_extensions(name)
+          "#{name}.html.haml"
+        end
      
-    end
-    p Factory.namespace
+      end
+      p Factory.namespace
     
+    end
   end
-end
 end
