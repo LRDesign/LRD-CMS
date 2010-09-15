9325c2cabc5bdf37c0ce81b9a6897e524a48e3c8
Evan Dorn <evan@lrdesign.com>
2010-09-15 12:17:20 -0700

    trial and error commit

--- lib/generators/rspec/scaffold/scaffold_generator.rb
+++ lib/generators/rspec/scaffold/scaffold_generator.rb
@@ -12,8 +12,8 @@ module Rspec
       # replace copy view with one that explicitly doesn't put the
       # template engine in the file name
       def copy_view(view)
-        p :source_paths => source_paths    
-        template "#{view}_spec.rb",
+        # p :source_paths => source_paths    
+        template "#{view}.html_spec.rb",
                  File.join("spec/views", controller_file_path, "#{view}.html_spec.rb")
       end      
     end        
