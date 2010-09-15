46b5fad85d713e33c0e1cbf53a962757df61e807
Evan <evan@lrdesign.com>
2010-09-14 17:16:45 -0700

    another namespace fix

--- lib/generators/lrd/haml/scaffold/scaffold_generator.rb
+++ lib/generators/lrd/haml/scaffold/scaffold_generator.rb
@@ -1,28 +1,30 @@
 module Lrd
   module Generators
-    class Haml < Rails::Generators::Base 
-      p "defining the haml scaffold generator"
+    module Haml 
+      class ScaffoldGenerator < Rails::Generators::Base 
+        p "defining the haml scaffold generator"
       
-      include Rails::Generators::ResourceHelpers
+        include Rails::Generators::ResourceHelpers
 
-      def create_root_folder
-        empty_directory File.join("app/views", controller_file_path)
-      end
-
-      def copy_view_files
-        available_views.each do |view|
-          filename = filename_with_extensions(view)
-          template filename, File.join("app/views", controller_file_path, filename)
+        def create_root_folder
+          empty_directory File.join("app/views", controller_file_path)
         end
-      end
 
-      protected
-
-        def available_views
-          %w(index edit show new _form)
+        def copy_view_files
+          available_views.each do |view|
+            filename = filename_with_extensions(view)
+            template filename, File.join("app/views", controller_file_path, filename)
+          end
         end
-    end
+
+        protected
+
+          def available_views
+            %w(index edit show new _form)
+          end
+      end
   
-    p Haml.namespace
+      p Scaffold.namespace
+    end
   end
 end
