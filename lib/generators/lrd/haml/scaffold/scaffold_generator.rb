8f80c332afb820e0cf50e3ad6d34537b07d38d88
Evan <evan@lrdesign.com>
2010-09-14 17:05:24 -0700

    moved haml generator into lrd directory

---
+++ lib/generators/lrd/haml/scaffold/scaffold_generator.rb
@@ -0,0 +1,30 @@
+raise "loading lrd haml generator"
+
+module Lrd
+  module Generators
+    class Haml < Base 
+      p "defining the haml scaffold generator"
+      
+      include Rails::Generators::ResourceHelpers
+
+      def create_root_folder
+        empty_directory File.join("app/views", controller_file_path)
+      end
+
+      def copy_view_files
+        available_views.each do |view|
+          filename = filename_with_extensions(view)
+          template filename, File.join("app/views", controller_file_path, filename)
+        end
+      end
+
+      protected
+
+        def available_views
+          %w(index edit show new _form)
+        end
+    end
+  
+    p Haml.namespace
+  end
+end
