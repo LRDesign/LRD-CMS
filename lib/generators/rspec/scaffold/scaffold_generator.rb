421da759c68971710b78b0106240d96246780479
Evan Dorn <evan@lrdesign.com>
2010-09-15 11:13:14 -0700

    trial and error commit

--- lib/generators/rspec/scaffold/scaffold_generator.rb
+++ lib/generators/rspec/scaffold/scaffold_generator.rb
@@ -1,16 +1,13 @@
 require 'generators/rspec'
 require 'rails/generators/resource_helpers'
 
-module Rspec
-  module Generators
-    class ScaffoldGenerator
+class Rspec::Generators::ScaffoldGenerator
+  p "reopening rspec scaffold generator"
       
-      # replace copy view with one that explicitly doesn't put the
-      # template engine in the file name
-      def copy_view(view)
-        template "#{view}_spec.rb",
-                 File.join("spec/views", controller_file_path, "#{view}.html_spec.rb")
-      end      
-    end
-  end
+  # replace copy view with one that explicitly doesn't put the
+  # template engine in the file name
+  def copy_view(view)
+    template "#{view}_spec.rb",
+             File.join("spec/views", controller_file_path, "#{view}.html_spec.rb")
+  end      
 end
