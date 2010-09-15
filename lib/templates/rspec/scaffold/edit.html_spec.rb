e45fa7d4945c3d407cf074750f3c8d6ce362ec2e
Evan Dorn <evan@lrdesign.com>
2010-09-15 15:04:59 -0700

    another template fix

--- lib/templates/rspec/scaffold/edit.html_spec.rb
+++ lib/templates/rspec/scaffold/edit.html_spec.rb
@@ -14,7 +14,7 @@ describe "/<%= table_name %>/edit" do
   it "should render edit form" do
     render
     
-    rendered.should have_selector("form", :action => '<%= file_name %>_path(@<%= file_name %>)', :method=> 'post') do |form|
+    rendered.should have_selector("form", :action => <%= file_name %>_path(@<%= file_name %>), :method=> 'post') do |form|
 <% for attribute in attributes -%><% unless attribute.name =~ /_id/ || [:datetime, :timestamp, :time, :date].index(attribute.type) -%>
       form.should have_selector('<%= attribute.input_type -%>#<%= file_name %>_<%= attribute.name %>', :name => "<%= file_name %>[<%= attribute.name %>]")
 <% end -%><% end -%>
