d1377145b9017c42093ba27e8f847c054cfb342f
Oliver Newland <onewland@gmail.com>
2010-09-18 10:49:23 -0700

    Updated factories to create some spurious content

--- spec/factories/documents_factory.rb
+++ spec/factories/documents_factory.rb
@@ -1,2 +1,5 @@
 Factory.define :document, :class => Admin::Upload::Document do |document|
+  document.data_file_name 'dummy'
+  document.data_file_size 1
+  document.data_content_type 'fake/fake'
 end
--- spec/factories/images_factory.rb
+++ spec/factories/images_factory.rb
@@ -1,2 +1,5 @@
 Factory.define :image, :class => Admin::Upload::Image do |image|
+  image.image_file_name 'dummy'
+  image.image_file_size 1
+  image.image_content_type 'image/jpeg'
 end
