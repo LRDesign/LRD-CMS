3f47990c1d485aeac6bd654f635ac715179b26a4
Oliver Newland <onewland@gmail.com>
2010-09-22 12:08:09 -0700

    Add location/validation + valid factory

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,4 @@
 class Location < ActiveRecord::Base
-        
-
   # REMEMBER:  attr_accessible :attr1, :attr2 
   #
   # Logical Reality Projects set attr_accessible :nil by default for all 
@@ -9,11 +7,9 @@ class Location < ActiveRecord::Base
   # which can be a PITA to debug.  Please do this intelligently, and only for 
   # attributes that should be assignable from a web form.  Things like a 
   # User#admin boolean probably should not be accessible. :-)
-                                               
-  # TODO:  create a validation or two
-  # 
-  # The model needs a validation for the controller specs to be completed.
-  # you can use that then to set @valid_create_params and similar in
-  # the generated controller specs, and make
-  
+  attr_accessible :name, :path, :parent_id, :page_id                                               
+  belongs_to :page
+  belongs_to :parent, :class_name => 'Location'
+
+  validates_presence_of :name
 end
--- spec/factories/locations_factory.rb
+++ spec/factories/locations_factory.rb
@@ -1,2 +1,3 @@
 Factory.define :location do |location|
+  location.name 'test'
 end
