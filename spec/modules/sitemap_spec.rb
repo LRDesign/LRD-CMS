390ea951652cb4f421814987590488799d33f6b6
Oliver Newland <onewland@gmail.com>
2010-10-09 13:33:42 -0700

    Sanity testing for sitemap

---
+++ spec/modules/sitemap_spec.rb
@@ -0,0 +1,20 @@
+require 'sitemap'
+
+describe Sitemap do
+  describe "with no pages" do 
+    it "should not crash" do
+      Sitemap.create!('foobar')
+    end
+  end
+ 
+  describe "with a couple pages" do
+    before(:each) do
+      @page_public = Factory(:page, :published => true)
+      @page_private = Factory(:page, :published => false)
+    end
+
+    it "should not crash" do
+      Sitemap.create!('foobar')
+    end
+  end
+end
