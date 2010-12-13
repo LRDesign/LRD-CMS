91deb729bb1e41d8e7d7a039406c587cd5366169
Evan Dorn <evan@lrdesign.com>
2010-12-13 12:02:30 -0800

    attempt to fix occasional problem with config/initializers/image_styles.rb

--- config/initializers/image_styles.rb
+++ config/initializers/image_styles.rb
@@ -1,6 +1,8 @@
 # Use this to add more default processors to Admin::Upload::Image
 
 # Adding a new style (or removing an old one) requires a restart.
-LRD::ImageStyles = {
-  :thumb => "100x100#"
-}
+module LRD
+  ImageStyles = {
+    :thumb => "100x100#"
+  }
+end
