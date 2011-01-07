f55dc4830c9f2323e57fc04d1280fc37ec60eddc
Evan Dorn <evan@lrdesign.com>
2011-01-07 15:48:12 -0800

    specced page routing, removed some other unused files

--- spec/routing/pages_routing_spec.rb
+++ spec/routing/pages_routing_spec.rb
@@ -3,7 +3,10 @@ require 'spec_helper'
 describe PagesController do
   describe "routing" do
     it "recognizes and generates #show" do
-      pending "Factory for pages"
+      { :get => "/some_page" }.should route_to(:controller => "pages", :action => "show", :permalink => 'some_page')
+    end
+    it "recognizes and generates #show with complex path" do
+      { :get => "/some/page" }.should route_to(:controller => "pages", :action => "show", :permalink => 'some/page')
     end
   end
 end
