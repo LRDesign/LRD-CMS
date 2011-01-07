a578d31d1ee536f8f6cb29c2ba7d71a90b940c7d
Evan Dorn <evan@lrdesign.com>
2011-01-07 15:24:42 -0800

    added some specs to location model

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Location do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "mass assignment" do
+     it "should mass assign name and path" do
+       location = Location.new(:name => 'foo', :path => 'bar' )
+       location.name.should == 'foo'
+       location.path.should == 'bar'
+     end
+   end
+
+   describe "validations" do
+     describe "presence" do
+       it "should not create a location with blank name" do
+         Factory.build(:location, :name => nil).should_not be_valid
+       end
+     end
+   end
 end
