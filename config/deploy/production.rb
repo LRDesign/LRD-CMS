7d99d700523fd2a6607b78ff9699abea20405681
Evan Dorn <evan@lrdesign.com>
2013-04-12 14:47:17 -0700

    [#48006897] Deploy config files.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,11 +1,11 @@
-set :user, 'lrd'
-set :domain, 'lrd-cms.lrdesign.com'
-set :applicationdir, "/home/#{user}/#{domain}"
+set :user, 'root'
+set :domain, 'appserver.lrdesign.com'
+set :applicationdir, "/var/www/some-cms-server.com"
 set :deploy_to, applicationdir
-set :keep_releases, 10                
-set :branch, 'master'         
+set :keep_releases, 10
+set :branch, 'master'
 
-set :rails_env, "production"        
+set :rails_env, "production"
 set :chmod755, "app config db lib public vendor script script/* public/disp*"
 set :use_sudo, false
 
