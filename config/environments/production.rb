840560de091a216364a96ad857c4ed936932a2d1
Evan Dorn <evan@lrdesign.com>
2013-04-12 17:04:25 -0700

    Set cache store to memory store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,5 @@ LrdCms::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.cache_store = :memory_store
 end
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -43,4 +43,5 @@ LrdCms::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.cache_store = :memory_store
 end
