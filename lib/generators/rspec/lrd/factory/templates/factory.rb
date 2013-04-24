398003be35368f00d2838f3072a157181291a474
Evan Dorn <evan@lrdesign.com>
2013-04-24 16:20:22 -0700

    Remove old lrd scaffold generator templates

--- lib/generators/rspec/lrd/factory/templates/factory.rb
+++
@@ -1,5 +0,0 @@
-Factory.define :<%= singular_name %> do |<%= singular_name %>|
-  <%- for attribute in attributes -%>
-  <%= singular_name %>.<%= attribute.name %> <%= attribute.default_factory_value %>
-  <%- end -%>
-end
