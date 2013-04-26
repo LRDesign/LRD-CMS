265c97985667e83265385cabf4c65ef4181eed53
Hannah Howard <hannah@hannahhoward.net>
2013-04-26 14:35:54 -0700

    Fixes for file paths

--- app/uploaders/doc_uploader.rb
+++ app/uploaders/doc_uploader.rb
@@ -2,4 +2,9 @@
 
 class DocUploader < CarrierWave::Uploader::Base
   include CarrierWave::Compatibility::Paperclip
+
+  def store_dir
+    ":rails_root/public/system/:class/:attachment/:id_partition"
+  end
+  
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,6 +4,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::Compatibility::Paperclip
   include CarrierWave::MiniMagick
   
+  def store_dir
+    ":rails_root/public/system/:class/:attachment/:id_partition"
+  end
+
   version :thumb do
     process :resize_to_fill => [100, 100]
   end
