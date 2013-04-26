69a12ad5da0830c3d9cc9e94bb2afb5973ede26b
Hannah Howard <hannah@hannahhoward.net>
2013-04-26 15:27:56 -0700

    Final fixes for specs

--- app/uploaders/doc_uploader.rb
+++ app/uploaders/doc_uploader.rb
@@ -1,10 +1,4 @@
 # encoding: utf-8
 
-class DocUploader < CarrierWave::Uploader::Base
-  include CarrierWave::Compatibility::Paperclip
-
-  def store_dir
-    ":rails_root/public/system/:class/:attachment/:id_partition"
-  end
-  
+class DocUploader < PaperclipUploader
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,12 +1,7 @@
 # encoding: utf-8
 
-class ImageUploader < CarrierWave::Uploader::Base
-  include CarrierWave::Compatibility::Paperclip
+class ImageUploader < PaperclipUploader
   include CarrierWave::MiniMagick
-  
-  def store_dir
-    ":rails_root/public/system/:class/:attachment/:id_partition"
-  end
 
   version :thumb do
     process :resize_to_fill => [100, 100]
