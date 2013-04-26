b2e3892269cf11c27ded9e61520ec43f161c0095
Hannah Howard <hannah@hannahhoward.net>
2013-04-26 15:30:07 -0700

    Remove file

--- lib/paperclip_path_fixer.rb
+++
@@ -1,8 +0,0 @@
-module PaperclipPathFixer
-  def self.included(base)
-    if Rail.env != "test"
-      base.define_method :paperclip_path do
-        ":rails_root/public/system/:class/:attachment/:id_partition/:style/:basename.:extension"
-      end
-    end
-  end
