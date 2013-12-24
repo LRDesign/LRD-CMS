dc2f8dab66a85fd4b5c8c02d70555fc1df9b914b
Evan Dorn <evan@lrdesign.com>
2013-12-24 14:45:17 -0700

    add eager_load config setting in the app

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,6 +6,7 @@ LrdCms::Application.configure do
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs.  Don't rely on the data there!
   config.cache_classes = true
+  config.eager_load = false
 
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
