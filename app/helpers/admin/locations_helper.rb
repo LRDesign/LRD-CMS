de848a8a9f595ffa31e8bba63b082953ad814ada
Evan Dorn <evan@lrdesign.com>
2013-12-24 12:00:37 -0700

    Fix some depration warnings with find(:all) in locations_helper

--- app/helpers/admin/locations_helper.rb
+++ app/helpers/admin/locations_helper.rb
@@ -1,8 +1,8 @@
 module Admin::LocationsHelper
   def location_parent_selector(form)
-    form.select(:parent_id, 
-      Location.find(:all).collect  {|p| [p.name, p.id]}, { :include_blank => "" }
-    )     
+    form.select(:parent_id,
+      Location.all.collect  {|p| [p.name, p.id]}, { :include_blank => "" }
+    )
   end
 
   def sibling_selector(form)
@@ -15,25 +15,25 @@ module Admin::LocationsHelper
 
   def  page_selector(form)
     form.select(:page_id,
-      Page.find(:all).collect { |c| [c.title, c.id]}, { :include_blank => "" }
-    )    
+      Page.all.collect { |c| [c.title, c.id]}, { :include_blank => "" }
+    )
   end
 
   def list_tree(locations, add_depth = 0, selected_location_id = nil)
     str = ''
     [*locations].each do |location|
       Location.each_with_level(location.self_and_descendants) do |location, depth|
-        str << (render :partial => "location", :locals => { :location => location, 
-                                                            :depth => (depth + add_depth), 
+        str << (render :partial => "location", :locals => { :location => location,
+                                                            :depth => (depth + add_depth),
                                                             :selected_location_id => selected_location_id})
       end
     end
     str.html_safe
-  end       
+  end
 
   def depth_indicator(depth)
     str = ''
-    if depth > 0 
+    if depth > 0
       depth.times do
         str << image_tag("/images/icons/spacer.png", :class=> "inline", :size =>"20x12", :alt => "&nbsp;&nbsp;&nbsp;")
       end
