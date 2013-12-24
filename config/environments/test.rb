2b68be4e92c10d35db6d9c09d47833d778962986
Evan Dorn <evan@lrdesign.com>
2013-12-24 14:46:47 -0700

    Remove whiny_nils setting from test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,8 +8,6 @@ LrdCms::Application.configure do
   config.cache_classes = true
   config.eager_load = false
 
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
