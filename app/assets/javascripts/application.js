8b47a66c46337b0cf316c99109bc39b3946120f2
Judson <nyarly@gmail.com>
2014-01-21 16:49:07 -0800

    Two character sprockets fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,5 @@
 //= require common/ninjascript
-//= require_tree ninjascripts/
+//= require_tree ./ninjascripts/
 //= require ninja-go
 //= require common/logical_tabs_jquery
 //= require tinymce-jquery
