7efb4c4a2d594f3f62f56bdb9923bec28c77e6db
Hannah Howard <hannah@hannahhoward.net>
2014-07-25 11:23:40 -0700

    Running rails update script

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,6 @@
 require 'deadbeat_connection_release'
 
-LrdCms::Application.configure do
+Rails.application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
 
   # The test environment is used exclusively to run your application's
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-LrdCms::Application.routes.draw do
+Rails.application.routes.draw do
 
   namespace :admin do
     resources :images
