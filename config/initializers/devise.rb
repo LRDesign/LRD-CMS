21fcb08a7e9e1996a5a33bec917fb57413159e6b
Evan Dorn <evan@lrdesign.com>
2015-06-22 11:42:01 -0700

    Import secret key from secrets.yml into devise config.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,6 +5,8 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
   config.mailer_sender = "tracks@lrdesign.com"
+  config.secret_key = Rails.application.secrets.secret_key_base
+
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
